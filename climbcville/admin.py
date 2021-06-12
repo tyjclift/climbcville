882bdd428eb5fdfc629bdd2d8f9bd213ffe854a2
Tyler Clift <tyjclift@gmail.com>
2021-06-11 21:31:03 -0400

    added all models (tables) and relationships for database

--- climbcville/admin.py
+++ climbcville/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Climber, Route_Setter, Admin, Route, Route_Log_Entry, Location
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Climber)
+admin.site.register(Route_Setter)
+admin.site.register(Admin)
+admin.site.register(Route)
+admin.site.register(Route_Log_Entry)
+admin.site.register(Location)
--- climbcville/models.py
+++ climbcville/models.py
@@ -11,10 +11,61 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class Person(models.Model):
+    person_id = models.IntegerField(primary_key=True)
+    f_name = models.CharField(max_length=50)
+    l_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.IntegerField()
+    def __str__(self):
+        return self.person_id
+
+    # see abstract base classes, meta inheritance
+    class Meta:
+        abstract = True
+    
+
+class Route_Setter(Person):
+    rs_experience_level = models.PositiveIntegerField() # quantitative for now
+    certifications = models.TextField() # comma separated? or create separate table for certifications?
+
+class Climber(Person):
+    c_experience_level = models.PositiveIntegerField() # quantitative for now
+    routes_attempted = models.PositiveIntegerField()
+    is_team_member = models.BooleanField()
+
+class Admin(Person):
+    is_owner = models.BooleanField()
+    edit_permission = models.BooleanField()
+    create_permission = models.BooleanField()
+    delete_permission = models.BooleanField()
+
+class Location(models.Model):
+    location_id = models.IntegerField(primary_key=True)
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+    approach_info = models.TextField()
+
+class Route(models.Model):
+    route_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    r_type = models.CharField(max_length=20) # ideally we would make this multiple choice - either by assigning integers or booleans
+    rs_grade = models.CharField(max_length=10)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    person_id = models.ManyToManyField(Route_Setter)
+
+class Route_Log_Entry(models.Model):
+    entry_id = models.IntegerField(primary_key=True)
+    comment = models.TextField()
+    beta = models.TextField()
+    c_grade = models.CharField(max_length=10)
+    c_rating = models.CharField(max_length=10)
+    rs_id = models.ForeignKey(Climber, on_delete=models.CASCADE, null=True)
+    route_id = models.ForeignKey(Route, on_delete=models.CASCADE, null=True)
