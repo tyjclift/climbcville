abae3f5e1d9a7f9db94906f74b0af4f0872725ea
Tyler Clift <tyjclift@gmail.com>
2021-06-12 14:47:13 -0400

    added all climbs with link to route_details page

--- climbcville/urls.py
+++ climbcville/urls.py
@@ -6,10 +6,12 @@ app_name = 'climbcville'
 urlpatterns = [
     # ex: /climbcville/
     path('', views.index, name='index'),
-    # ex: /climbcville/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /climbcville/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /climbcville/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # ex: /climbcville/question5/
+    path('question<int:question_id>/', views.poll_details, name='poll_details'),
+    # ex: /climbcville/route5/
+    path('route<int:route_id>/', views.route_details, name='route_details'),
+    # ex: /climbcville/question5/results/
+    path('question<int:question_id>/results/', views.results, name='results'),
+    # ex: /climbcville/question5/vote/
+    path('question<int:question_id>/vote/', views.vote, name='vote'),
 ]
